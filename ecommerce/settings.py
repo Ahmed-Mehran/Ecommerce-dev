51a5adab12ce5713b2ce6172e9b6d235ba66d565
Ahmed-Mehran <168980400+Ahmed-Mehran@users.noreply.github.com>
2024-07-06 21:29:42 +0530

    Commit #5

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ecommerce-dev-0yx4.onrender.com', '*']
 
-#CSRF_TRUSTED_ORIGINS = ['']
+CSRF_TRUSTED_ORIGINS = ['https://ecommerce-dev-0yx4.onrender.com']
 
 # Application definition
 
